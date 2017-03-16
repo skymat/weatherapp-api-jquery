db68db09f286a48e5cc935a9c0937d739ce94449
Mathieu Bourque <skymatb@gmail.com>
2017-03-16 18:14:35 +0100

    ajax

--- public/script.js
+++ public/script.js
@@ -1,5 +1,30 @@
 $("document").ready(function() {
   // DOM chargé 
 
-  
+  $("#city").keyup(function(event){ 
+
+        if($(this).val() != undefined)
+        {
+            if ($(this).val().length > 2)
+            {
+                $("#btValidCity").prop('disabled', false);
+                
+                console.log($(this).val().length);
+            }
+            else
+                $("#btValidCity").prop('disabled', true);
+        }
+    
+});
+
+ $("a").click(function(event){ 
+     $(this).parent().fadeOut(function(){
+        $.get( $(this).attr("href"), function( data ) {
+        });
+     });
+
+    
+ });
+
+
 })
