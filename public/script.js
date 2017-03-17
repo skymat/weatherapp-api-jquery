4a864dc94f2340291874c368dc23f4c182f0c567
Mathieu Bourque <skymatb@gmail.com>
2017-03-17 14:34:45 +0100

    Sort Post oldindex newindex

--- public/script.js
+++ public/script.js
@@ -1,6 +1,25 @@
 $("document").ready(function() {
   // DOM chargé 
 
+$( function() {
+    $( "#sortable" ).sortable({
+        start: function(e, ui) {
+        // creates a temporary attribute on the element with the old index
+        $(this).attr('data-previndex', ui.item.index());
+    },
+        update: function(e, ui) {
+        // gets the new and old index then removes the temporary attribute
+        var newIndex = ui.item.index();
+        var oldIndex = $(this).attr('data-previndex');
+        $(this).removeAttr('data-previndex');
+
+        $.post( "/sortList", { new_index : newIndex, old_index: oldIndex });
+    }
+    });
+});
+
+
+
   $("#city").keyup(function(event){ 
 
         if($(this).val() != undefined)
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // this is used for parsing the JSON object from POST
 
 var cityList = [
  
@@ -16,20 +18,31 @@ app.get('/', function (req, res) {
   });
 });
 
+app.post('/sortList', function (req, res) {
+  var new_index = req.body.new_index;
+  var old_index = req.body.old_index;
+
+  cityListMove(old_index,new_index);
+
+});
+
+function cityListMove(fromIndex, toIndex) {
+    var element = cityList[fromIndex];
+    cityList.splice(fromIndex, 1);
+    cityList.splice(toIndex, 0, element);
+}
+
 app.get('/add', function (req, res) {
   
   request("http://api.openweathermap.org/data/2.5/weather?q="+req.query.city+"&APPID=9b754f1f40051783e4f72c176953866e&units=metric&lang=fr", function(error, response, body) {
   
     body = JSON.parse(body);
     
-    console.log(req.query);
     req.query.city     = body.name;
     req.query.temp_min = body.main.temp_min;
     req.query.temp_max = body.main.temp_max;
     req.query.picto    = body.weather[0].icon;
     req.query.description = body.weather[0].description;
-    console.log(body.main.temp_min+'//'+body.main.temp_max+'//'+body.weather[0].description);
-    console.log(req.query);
     
     cityList.push(req.query);
   
